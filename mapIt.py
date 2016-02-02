9c09eb7bf0c6f2198ada6746b4148cbf34f561ae
Ido Shamriz <ido.shamriz@gmail.com>
2016-02-02 15:29:51 +0200

    First Python File

---
+++ mapIt.py
@@ -0,0 +1,12 @@
+import webbrowser, sys, pyperclip
+pyperclip.copy('hello, it\'s me')
+if len(sys.argv) > 1:
+	address = '+'.join(sys.argv[1:])
+else:
+	address = pyperclip.paste()
+webbrowser.open('https://www.google.com')
+webbrowser.open('https://www.youtube.com')
+webbrowser.open_new('https://mail.google.com')
+webbrowser.open('https://www.fxp.co.il')
+webbrowser.get('mozzila')
+webbrowser.open('https://www.google.com')
